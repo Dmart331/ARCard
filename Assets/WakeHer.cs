36e96492107dc38d5bf3aa83a94fe4c8a2b5ca53
Drew Martin <dmart331@gmail.com>
2017-07-18 08:53:43 -0500

    Initial commit. I believe this is the version with the avatar. The aspect ratio of the video on the wall is off and needs to be fixed.

---
+++ Assets/WakeHer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakeHer : MonoBehaviour {
+    public GameObject Avatar;
+
+	// Use this for initialization
+	void Start () {
+        Avatar.SetActive(false);
+        StartCoroutine(Wake());
+	}
+	
+    IEnumerator Wake()
+    {
+        yield return new WaitForSeconds(4);
+        Avatar.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
